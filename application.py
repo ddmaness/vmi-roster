c56d9d7bf3640bd27a6ad78c1b4a6eec28772627
Donald Maness <ddmaness@gmail.com>
2017-09-10 15:52:07 -0400

    verify new cadet to make sure fields are not blank and cadet is not already in database

--- application.py
+++ application.py
@@ -11,9 +11,23 @@ def index():
     if request.method ==  "POST":
         first = request.form.get("first")
         last = request.form.get("last")
-        c.execute("INSERT INTO cadets (first_name, last_name, attendance_current_rank, attendance_total) VALUES (?, ?, 1, 1)", (first, last))
-        conn.commit()
-        return redirect("/")
+        if first == "" or last == "":
+            msg = "please provide your first and last name"
+            return error(msg)
+        else:
+            lookup = c.execute("SELECT first_name, last_name FROM cadets WHERE first_name=? AND last_name=?", (first, last))
+            exists = c.fetchone()
+            if exists is None:
+                c.execute("INSERT INTO cadets (first_name, last_name, attendance_current_rank, attendance_total) VALUES (?, ?, 1, 1)", (first, last))
+                conn.commit()
+                return redirect("/")
+            else:
+                msg = "this name already exists in the database"
+                return error(msg)
     else:
         cadets = c.execute("SELECT * FROM cadets")
         return render_template("index.html", cadets = cadets)
+
+def error(msg):
+    return render_template("error.html", msg = msg)
+
